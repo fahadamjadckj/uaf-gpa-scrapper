9fd2912975c0111af1919e13a81f5bf2837a6ee2
martialdwarf <mohammadfahadamajd@gmail.com>
2022-08-18 12:36:57 -0700

    added no sandbox

--- utils/dataScraper.js
+++ utils/dataScraper.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer')
 
 async function getData(regNumber) {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  })
   const page = await browser.newPage()
   await page.goto('http://lms.uaf.edu.pk/login/index.php')
 
